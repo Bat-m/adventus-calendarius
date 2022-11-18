3b925bdeebf19d56005731571a0dc4eedcbf2697
Baptiste <53108208+Bat-m@users.noreply.github.com>
2022-11-18 01:09:15 +0100

    arrange css

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,7 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
   import db from './db'
   import { onMount } from 'svelte';
-  import { dataset_dev } from 'svelte/internal';
-
+  import snarkdown from 'snarkdown';
   let allCases: any=[];
   onMount(async() => {
     allCases = await db.cases.all()
@@ -26,6 +23,13 @@
 			selected = Number(e.target.dataset.cardId)
 		}
 	}
+  let md = '_this_ is **easy** to `use`.';
+var html: string 
+console.log(html);
+  const renderMd=(md:string)=>{
+    html = snarkdown(md)
+    return html
+  }
 </script>
 
 <main>
@@ -34,33 +38,36 @@
       <div class="flip-box">
         <div class="flip-box-inner" class:show-back={selected === day}>
           <!-- svelte-ignore a11y-click-events-have-key-events -->
-          <div class="flip-box-front card" on:click={(e)=>toggleBackFront(e)} data-card-id={day}>
+          <div class="flip-box-front card" on:click={(e)=>{toggleBackFront(e);renderMd(content)}} data-card-id={day}>
            {day}
           </div>
   
           <!-- svelte-ignore a11y-click-events-have-key-events -->
           <div class="flip-box-back container"  on:click={(e)=>toggleBackFront(e)} data-card-id={day}>
-            <div class="flex-shrink-0">
+            <div style="flex-shrink: 0; height:{type==="photo"?"50%":type==="musique"||type==="video"?"70%":"1%"};" >
+              {#if type==="story"}
+                  <img style="width:100%; height:0; object-fit:cover; border-top-left-radius: 0.5rem; border-top-right-radius: 0.5rem;" src={photo} alt="" />
+              {/if}
               {#if type==="photo"}
-                  <img class="h-full w-full object-cover" src={photo} alt="" />
+                  <img style="width:100%; height:100%; object-fit:cover; border-top-left-radius: 0.5rem; border-top-right-radius: 0.5rem;" src={photo} alt="" />
               {/if}
               {#if type==="musique"}
-                  <iframe width="300" height="315" src={link} title="yuyube"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                  <iframe width="200" height="200" src={link} title="yuyube"  frameborder="0" allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
               {/if}
               {#if type==="video"}
-                  <video  width="400" controls autoplay >
+                  <video  width="200" height="200" controls >
                     <track kind="captions">
                       <source src={link}
                               type="video/mp4">
                   </video>
               {/if}
           </div>
-          <div class="flex flex-1 flex-col justify-between bg-white p-6">
-              <div class="flex-1">
-                      <p class="text-sm font-medium text-indigo-600">{type}</p>
-                  <div class="mt-2 block">
-                      <p class="text-xl font-semibold text-gray-900"> {title}</p>
-                      <p class="mt-3 text-base text-gray-500">{content}</p>
+          <div style="display:flex; flex: 1; flex-direction: column; justify-content: space-between; background:white; border-bottom-left-radius: 0.5rem; border-bottom-right-radius: 0.5rem;">
+              <div style="flex:1;" >
+                      <p style="font-size: 0.875rem; line-height: 1.25rem; font-weight: 500; color:#4f46e5; margin:0;">{type}</p>
+                  <div style="display:block; margin:0; margin-top: 0.5rem; " >
+                      <p style="font-size: 1.25rem; line-height: 1.75rem; font-weight: 600; color:#111827; margin:0;" > {title}</p>
+                      <p style="margin:0; margin-top: 0.75rem; font-size: 1rem; line-height: 1rem; color:#6b7280;" >{@html html}</p>
                   </div>
               </div>
           </div>
@@ -75,6 +82,14 @@
 </main>
 
 <style>
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+@import url('https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+
+:global(body) {
+  font-family: 'Roboto', Arial, Helvetica, sans-serif;
+  background-color: #333;
+  line-height: 1.6;
+}
 	h1 {
 		margin: 0 0 5px;
 	}
@@ -198,9 +213,10 @@
 
 	/* Some left and right padding inside the container */
 	.container {
+    border-radius: 0.5rem;
 		/* padding: 0 5px 0 5px; */
-    padding-left: 2px;
-    padding-right: 5px;
+    margin-left: 5px;
+    margin-right: 5px;
 	}
 
 	/* Clear floats */
